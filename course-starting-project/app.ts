051a53a8a82192ccb37e6b6a10aaf516ff8842e2
Vignan Anil Sunkara <vignan.ifocus@gmail.com>
2022-03-29 11:50:15 +0530

    changes

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,9 +1,13 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
-}
+// const person: {
+//   name: string;
+//   age: number;
+//   nickname: string;
+// } = {
+const person = {
+  name: "Max",
+  age: 32,
+  nickname: "m",
+  hobbies: ["Sports", "Cooking"],
+};
 
-const number1 = "5";
-const number2 = 2.8;
-
-const result = add(+number1, +number2);
-console.log(result);
+console.log(person.name);
